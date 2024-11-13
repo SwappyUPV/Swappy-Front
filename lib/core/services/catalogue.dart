8912b177194aa4552976ed3b57129f0fb95791c4
Francisco He Chi <fhchi2003@gmail.com>
2024-11-13 12:16:38 +0100

    Created method in service to get Clothes by UserId

--- lib/core/services/catalogue.dart
+++ lib/core/services/catalogue.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pin/features/exchanges/models/Product.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CatalogService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -59,5 +61,50 @@ class CatalogService {
       print('Error al obtener la prenda: $e');
       return null;
     }
+  }
+
+  Future<List<Product>> getClothByUserId(String id) async {
+    try {
+      QuerySnapshot querySnapshot =
+          await _firestore.collection('clothes').where("userId", isEqualTo: id).get();
+
+      List<Product> clothes = querySnapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return Product(
+          id: doc.id,
+          title: data['nombre'] ?? '',
+          price: data['precio'] ?? 0,
+          image: data['imagen'] ?? '',
+          description: data['descripcion'] ?? '',
+          size: data['talla'] ?? '',
+          styles: List<String>.from(data['styles'] ?? []),
+          quality: data['quality'] ?? '',
+          category: data['categoria'] ?? '',
+          isExchangeOnly: data['isExchangeOnly'] ?? false,
+          color: null,
+        );
+      }).toList();
+
+      return clothes;
+    } catch (e) {
+      print('Error al obtener la ropa: $e');
+      rethrow;
+    }
+  }
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // Cache the authenticated user's ID to reduce duplicate calls
+  String? _cachedUserId;
+
+  Future<String?> getUserId() async {
+    if (_cachedUserId == null) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      _cachedUserId = prefs.getString('userId') ?? _auth.currentUser?.uid;
+
+      if (_cachedUserId != null) {
+        await prefs.setString('userId', _cachedUserId!);
+      }
+    }
+    return _cachedUserId;
   }
 }
