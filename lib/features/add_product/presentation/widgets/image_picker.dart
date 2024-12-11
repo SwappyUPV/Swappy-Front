74bd4e6d30908042005d3e1fe60c50b9d5505bbc
Shiyi <scheng1@upv.edu.es>
2024-12-11 02:26:41 +0100

    Cambios en subir foto

--- lib/features/add_product/presentation/widgets/image_picker.dart
+++ lib/features/add_product/presentation/widgets/image_picker.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:http/http.dart' as http;
 import 'dart:io';
 
-class ImagePickerWidget extends StatelessWidget {
+class ImagePickerWidget extends StatefulWidget {
   final dynamic pickedImage;
   final Function(dynamic) onImagePicked;
 
@@ -13,6 +14,13 @@ class ImagePickerWidget extends StatelessWidget {
     required this.onImagePicked,
   });
 
+  @override
+  State<ImagePickerWidget> createState() => _ImagePickerWidgetState();
+}
+
+class _ImagePickerWidgetState extends State<ImagePickerWidget> {
+  bool _isProcessing = false;
+
   Future<void> _pickImage() async {
     try {
       final pickedFile = await ImagePicker().pickImage(
@@ -21,11 +29,63 @@ class ImagePickerWidget extends StatelessWidget {
         maxHeight: 1000,
         imageQuality: 70,
       );
+
       if (pickedFile != null) {
-        if (kIsWeb) {
-          onImagePicked(pickedFile);
-        } else {
-          onImagePicked(File(pickedFile.path));
+        setState(() => _isProcessing = true);
+
+        try {
+          var uri = Uri.parse('http://localhost:8000/remove-background');
+          var request = http.MultipartRequest('POST', uri);
+
+          if (kIsWeb) {
+            List<int> bytes = await pickedFile.readAsBytes();
+            request.files.add(
+              http.MultipartFile.fromBytes(
+                'image',
+                bytes,
+                filename: pickedFile.name,
+              ),
+            );
+          } else {
+            request.files.add(
+              await http.MultipartFile.fromPath(
+                'image',
+                pickedFile.path,
+              ),
+            );
+          }
+
+          var streamedResponse = await request.send();
+          var response = await http.Response.fromStream(streamedResponse);
+
+          if (response.statusCode == 200) {
+            if (kIsWeb) {
+              // Para web, crear un Blob URL
+              final blob = response.bodyBytes;
+              widget.onImagePicked(blob);
+            } else {
+              // Para móvil, guardar en archivo temporal
+              final tempDir = await Directory.systemTemp.createTemp();
+              final tempFile = File('${tempDir.path}/processed_image.png');
+              await tempFile.writeAsBytes(response.bodyBytes);
+              widget.onImagePicked(tempFile);
+            }
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content:
+                    Text('Error al procesar la imagen: ${response.statusCode}'),
+              ),
+            );
+          }
+        } catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Error al comunicarse con el servidor: $e'),
+            ),
+          );
+        } finally {
+          setState(() => _isProcessing = false);
         }
       }
     } catch (e) {
@@ -39,22 +99,30 @@ class ImagePickerWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         ElevatedButton.icon(
-          onPressed: _pickImage,
-          icon: Icon(Icons.image),
-          label: Text('Seleccionar Imagen'),
+          onPressed: _isProcessing ? null : _pickImage,
+          icon: _isProcessing
+              ? SizedBox(
+                  width: 24,
+                  height: 24,
+                  child: CircularProgressIndicator(strokeWidth: 2),
+                )
+              : Icon(Icons.image),
+          label: Text(_isProcessing ? 'Procesando...' : 'Seleccionar Imagen'),
         ),
-        if (pickedImage != null)
+        if (widget.pickedImage != null)
           Container(
             margin: EdgeInsets.only(top: 10),
-            width: 100,
-            height: 100,
+            width: 200,
+            height: 200,
             decoration: BoxDecoration(
               border: Border.all(color: Colors.grey),
               borderRadius: BorderRadius.circular(8),
             ),
             child: kIsWeb
-                ? Image.network(pickedImage.path)
-                : Image.file(pickedImage),
+                ? (widget.pickedImage is List<int>
+                    ? Image.memory(widget.pickedImage)
+                    : Image.network(widget.pickedImage.path))
+                : Image.file(widget.pickedImage),
           ),
       ],
     );
